95aff5248b027ebc95663b69b672a0cd6073c0f2
kiran <kiranjha.spearmint@gmail.com>
2023-07-10 13:34:31 +0530

    onlt work on specified chain

--- src/App.js
+++ src/App.js
@@ -17,29 +17,47 @@ function App() {
     const connectWallet = async() => {
       const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS; //0xc5D85ED6EFC84e823497521e778E30f79F16e6e8
       const contractABI = abi.abi;
-      try {
-        const {ethereum} = window;
-        if(ethereum) {
-          const account = await ethereum.request({method: 'eth_requestAccounts'});
-          window.ethereum.on("chainChanged",()=>{
-            window.location.reload();
-          });
-          window.ethereum.on("accountsChanged",()=>{
-            window.location.reload();
-          });
-          const provider = new ethers.providers.Web3Provider(ethereum);
-          const signer = provider.getSigner();
-          const contract = new ethers.Contract(contractAddress, contractABI, signer);
-          setAccount(account);
-          setState({provider, signer, contract});
-        } else {
-          alert("Install Metamask First");
+        try {
+          const {ethereum} = window;
+          if(ethereum) {
+            console.log(window.ethereum.networkVersion,"network id");
+            const account = await ethereum.request({method: 'eth_requestAccounts'});
+            if(window.ethereum.networkVersion == process.env.REACT_APP_CHAIN_ID) {
+              window.ethereum.on("chainChanged",()=>{
+                window.location.reload();
+              });
+              window.ethereum.on("accountsChanged",()=>{
+                window.location.reload();
+              });
+              const provider = new ethers.providers.Web3Provider(ethereum);
+              const signer = provider.getSigner();
+              const contract = new ethers.Contract(contractAddress, contractABI, signer);
+              setAccount(account);
+              setState({provider, signer, contract});
+            } else if (window.ethereum.networkVersion != process.env.NEXT_PUBLIC_CHAIN_ID) {
+              alert("Please switch to Fantom Testnet")
+            }
+          } else {
+            alert("Install Metamask First");
+          }
+        } catch(error) {
+          console.log("ERROR ",error);
         }
-      } catch(error) {
-        console.log("ERROR ",error);
-      }
     };
+    const handleNetworkChange = () => {
+      window.location.reload()
+    }
+    const handleAccountsChange = () => {
+      window.location.reload()
+    }
     connectWallet();
+    window.ethereum.on("networkChanged", handleNetworkChange)
+    window.ethereum.on("accountsChanged", handleAccountsChange)
+
+    return () => {
+      window.ethereum.removeListener("networkChanged", handleNetworkChange)
+      window.ethereum.removeListener("accountsChanged", handleAccountsChange)
+    }
   },[]);
   return (
     <div style={{background: "#EFEFEF", height: "90vh", background: "linear-gradient(to right, #d3cce3, #e9e4f0)"}}>
